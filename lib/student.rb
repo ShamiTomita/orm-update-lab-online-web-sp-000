1132ab40f264148d684954886511f188cdd70c63
Shami Tomita <shamitomita@gmail.com>
2021-04-05 19:36:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,10 @@ require_relative "../config/environment.rb"
 
 class Student
 attr_accessor :id, :name, :grade
+
+def initialize
+  @i
+end 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
