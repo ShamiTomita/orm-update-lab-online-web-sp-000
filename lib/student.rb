2dc88e6f1916a0ac822c21284d35a644ade74ac6
Shami Tomita <shamitomita@gmail.com>
2021-04-05 19:43:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,7 +33,7 @@ class Student
     VALUES (?,?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT last")
+    @id = DB[:conn].execute("SELECT last_insert_row")
   end 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
