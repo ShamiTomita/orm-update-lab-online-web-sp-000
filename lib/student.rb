c1f340245d48f1b4d46090f10a7d7d19f7303ad5
Shami Tomita <shamitomita@gmail.com>
2021-04-05 19:43:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,6 +33,7 @@ class Student
     VALUES (?,?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn]
   end 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
