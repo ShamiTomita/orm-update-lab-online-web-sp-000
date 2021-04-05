0fb1ce823fdd89d5ef1c36cb5a19262e5c2e70cb
Shami Tomita <shamitomita@gmail.com>
2021-04-05 19:50:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,6 +59,8 @@ class Student
     result = DB[:conn].execute(sql, name)[0]
     Student.new(result[0], result[1], result[2])
   end 
+  
+  def update
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
