0ce36d34923df1bcd6aa247816c2513da08cf00f
Shami Tomita <shamitomita@gmail.com>
2021-04-05 19:46:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,6 +45,10 @@ class Student
     student.save 
     student
   end 
+  
+  def self.new_from_db 
+    
+  end 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
