039bb5801fa96ac88f6f804c8699f18962b4817a
Shami Tomita <shamitomita@gmail.com>
2021-04-05 19:36:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,7 @@ class Student
 attr_accessor :id, :name, :grade
 
 def initialize
-  @i
+  @id = id 
 end 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
