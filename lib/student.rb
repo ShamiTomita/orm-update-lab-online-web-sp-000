d574ba27cc622d3283785e422854fa4ffe70d333
Shami Tomita <shamitomita@gmail.com>
2021-04-05 19:39:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,7 +17,7 @@ class Student
     grade TEXT
     )
     SQL
-  DB[:conn]
+   DB[:conn].execute(sql)
   end 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
