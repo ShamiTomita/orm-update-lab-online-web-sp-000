227eb529209c39848695966bb58c73d3201d5323
Shami Tomita <shamitomita@gmail.com>
2021-04-05 19:48:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,7 +55,7 @@ class Student
   end
   
   def self.find_by_name 
-    sql = 
+    sql = "SELECT * FROM "
   end 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
